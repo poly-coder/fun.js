7ad937611c3211d0b61bb3f6a4292a05408347fc
Iskander Sierra <iskander.sierra@gmail.com>
2020-05-10 16:51:26 +0200

    Simplified build steps

--- rollup.cjs.config.js
+++
@@ -1,8 +0,0 @@
-import { createOptions } from "./rollup.base-config";
-
-const config = createOptions("tsconfig.json", {
-  format: 'cjs',
-  dir: 'dist/cjs',
-})
-
-export default config
--- rollup.esm.config.js
+++
@@ -1,8 +0,0 @@
-import { createOptions } from "./rollup.base-config";
-
-const config = createOptions("tsconfig.esm.json", {
-  format: 'esm',
-  dir: 'dist/esm',
-})
-
-export default config
