b7d9b4f8823ac0844c5e029f69b80f5fcd53ecb4
Iskander Sierra <iskander.sierra@gmail.com>
2020-05-10 17:20:34 +0200

    Done configuring build

--- rollup.config.js
+++ rollup.config.js
@@ -4,26 +4,19 @@ import pkg from './package.json';
 
 const external = Object.keys(pkg.dependencies || {});
 
-const outOptions = {
-  // dir: 'dist',
-};
-
 export default {
   input: 'src/index.ts',
   external,
   output: [
     {
-      ...outOptions,
       file: pkg.main,
       format: 'cjs'
     },
     {
-      ...outOptions,
       file: pkg.module,
       format: 'es' // the preferred format
     },
     {
-      ...outOptions,
       file: pkg.browser,
       format: 'iife',
       name: 'PolyCoderFun' // the global which can be used in a browser
